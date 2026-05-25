274e6a35ba66348963825d4d2dce723f2e3e5e9e
andrehsilva <andrehrsilva@gmail.com>
2026-05-25 19:30:47 -0300

    tenant_ajustes

--- a_tenant_manager/admin.py
+++ a_tenant_manager/admin.py
@@ -3,7 +3,8 @@ from .models import Tenant, Domain
 
 @admin.register(Tenant)
 class TenantAdmin(admin.ModelAdmin):
-    list_display = ('schema_name', 'name', 'created_on')
+    # Alterado de 'created_on' para 'created'
+    list_display = ('schema_name', 'name', 'created')  
     search_fields = ('schema_name', 'name')
 
 @admin.register(Domain)
